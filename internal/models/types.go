d21a00e1d3ae567b8e1b966986cc3ecd54164a64
4yushraman-jpg <4yushraman@gmail.com>
2026-05-21 22:26:44 +0530

    campaign request and email job

--- internal/models/types.go
+++ internal/models/types.go
@@ -4,10 +4,12 @@ type CampaignRequest struct {
 	CampaignID string `json:"campaign_id"`
 	TemplateID string `json:"template_id"`
 	AudienceID string `json:"audience_id"`
+	Subject    string `json:"subject"`
 }
 
 type EmailJob struct {
 	JobID        string `json:"job_id"`
 	EmailAddress string `json:"email_address"`
 	TemplateID   string `json:"template_id"`
+	Subject      string `json:"subject"`
 }
